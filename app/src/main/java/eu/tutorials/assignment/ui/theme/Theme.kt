2e99c0d708e60cfd2c15200f404e54607afc167f
amEya <ameyakulkarni2023@gmail.com>
2024-11-05 20:53:17 +0530

    Initial

--- app/src/main/java/eu/tutorials/assignment/ui/theme/Theme.kt
+++ app/src/main/java/eu/tutorials/assignment/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package eu.tutorials.assignment.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
